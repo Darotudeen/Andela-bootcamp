45f4032beeee0c48c73cafab14e736debaa92567
darot peacedude <darotudeen@gmail.com>
2016-05-18 19:01:27 +0100

    committing testspec

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,8 +1,13 @@
 describe ("NotesApplication", function(){
-	it("takes in two parameter" function(){
+	it("should accept two argument" function(){
 		expect(this.author("")).toBe(false);
 		expect(this.notelist([])).toBeUndefined();
 	});
-
-	it("should ")
+	it("should save Notescontent" function(){
+		expect(NotesApplication.prototype.hasOwnProperty("Notescontent")).toBeUndefined;
+	});
+	it("should display notelist" function(){
+		expect(NotesApplication.prototype.hasOwnProperty("listNotes")).toBeUndefined;
+	});
+	
 }
