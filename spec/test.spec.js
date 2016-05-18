d888a1a2da28d9961cf91b55c14dab7f3a4408df
darot peacedude <darotudeen@gmail.com>
2016-05-18 17:59:41 +0100

    commtting spec file

---
+++ spec/test.spec.js
@@ -0,0 +1,8 @@
+describe ("NotesApplication", function(){
+	it("takes in two parameter" function(){
+		expect(this.author("")).toBe(false);
+		expect(this.notelist([])).toBeUndefined();
+	});
+
+	it("should ")
+}
