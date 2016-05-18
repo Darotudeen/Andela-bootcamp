ba2901226ac4ff48dca8cd97470e7c7102d3b8fe
darot peacedude <darotudeen@gmail.com>
2016-05-18 05:43:28 +0100

    sixth commit

--- oop.js
+++ oop.js
@@ -10,14 +10,28 @@ NotesApplication.prototype = {
 	NotesContent:function (note_content){
 		this.noteslist.push(note_content)
 	},
+	
 	listNotes :function(){
-		for ()
-		var NotesID = this.noteslist[i];
+		var Note_ID = this.noteslist[j];
+		if(this.noteslist.length>0){
+			for(j = 0; j<this.noteslist.length; j++){
+				console.log("Note_ID: " + Note_ID );
+				console.log(note_content);
+				console.log("By Author: " + this.author); 
+			}
+		}
 		
+
 	},
 
-	
+	get: function(Note_ID){
+		return this.noteslist.join(',');
 	}
+
+
+
+	
+}
 
 	
 
