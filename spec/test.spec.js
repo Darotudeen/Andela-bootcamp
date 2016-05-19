97d2c04a890c4b40547f1b83fbf1c017ba960a19
darot peacedude <darotudeen@gmail.com>
2016-05-19 16:02:26 +0100

    Committing spec and src files

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,45 +1,34 @@
 describe ('NotesApplication', function(){
 
-	it('it should add notecontent to noteslist', function(){
+	it('should add notecontent to noteslist', function(){
 		var note_content = "";
-		Book = new NotesApplication();
-		expect(Book.NotesContent).toIncrement(Book.noteslist);
+		var Book = new NotesApplication.prototype();
+		expect(Book.create(typeof "note_content").toBeDefined();
 	});
 
-	it('it should arrange each notes in noteslist', function(){
+	it('should arrange each notes in noteslist', function(){
 		var Note_ID = this.notelist[i];
-		Book = new NotesApplication();
-		expect(Book.NotesContent()).toIncrement(Book.noteslist);
+		var Book = new NotesApplication.prototype();
+		expect(Book.noteslist.length).toBe(0);
+		expect(Book.listNotes()).toBeDefined()
 	
 	});
 
 
-	it('it should return note from its index', function(){
-		var Note_ID = "";
-		Book = new NotesApplication();
-		expect(Book.NotesContent()).toIncrement(Book.noteslist);
-	
-	it('not null', function(){
-		var listNotes = function(){
-			service.listNotes(null);
-		}
-		expect(listNotes).not.toBe(null);
-	});
-
-	it('not null', function(){
-		var get = function(){
-			service.get(null);
-		}
-		expect(get).not.toBe(null);
-	});
-
-	it('not null', function(){
-		var search = function(){
-			service.onSearch(null);
-		}
-		expect(search).not.toBe(null);
+	it('should return note_content as string', function(){
+		var Note_ID = this.notelist[i];
+		Book = new NotesApplication.prototype();
+		expect(Book.get(Note_ID)).toBeDefined;
 	});
 	
+	it('should return notes with a search string', function(){
+		expect (str.search("text")).toBeDefined
+	});
 	
 	
 });
+
+	
+	
+	
+	
